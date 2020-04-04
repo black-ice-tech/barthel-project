bdbe39fde89f162e252a4c91256cf1091974bc31
Zack Schrag <zackschrag@gmail.com>
2020-04-04 16:05:58 -0600

    Project skeleton

---
+++ src/Barthel.Project.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Barthel.Project.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
