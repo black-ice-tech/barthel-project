8029bcc9669e61d96d600765869b67049e615c90
abarthel4 <52982540+abarthel4@users.noreply.github.com>
2020-04-06 19:59:40 -0700

    Add Sentence (#2)

--- src/Barthel.Project.Cli/Program.cs
+++ src/Barthel.Project.Cli/Program.cs
@@ -7,6 +7,7 @@ namespace Barthel.Project.Cli
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Zack!");
+            Console.WriteLine("It's Andrew! Your bestie!");
         }
     }
 }
